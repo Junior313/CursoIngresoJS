672d68ba1a0f1927855056044d40ae30fb594c45
Junior313 <joaquin_88@hotmail.com>
2017-06-28 20:32:15 -0300

    Primer commit
    Subimos los 2 primeros ejercicios

---
+++ jsEntradaSalida-2.js
@@ -0,0 +1,8 @@
+/*Debemos lograr tomar un nombre con 'prompt' 
+y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
+function Mostar()
+{
+	var nombre = prompt("Por favor ingresa tu nombre", "Natalia natalia");
+	alert(" Su nombre es: " + nombre);
+}
+
