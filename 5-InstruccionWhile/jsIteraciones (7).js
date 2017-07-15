6e9758944e9b051fc90bd7ced7e0d889570848c7
Junior313 <joaquin_88@hotmail.com>
2017-07-14 22:09:36 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -2,11 +2,40 @@ function Mostrar()
 {
 
 	var contador=0;
-	var acumulador=0;
-	var respuesta='si';
+	//var acumulador=0;
+	//var respuesta='si';
+	var nota;
+	var notamayor=0;
+	var notaminima=0;
+	
+while(contador<3)
+{
+	contador=contador+1;
+	console.log("vueltas" + contador);
+	nota=prompt("ingrese nota");
+	if(nota>notamayor)
+	{
+		notamayor=nota;
+		notaminima=nota;
+	}
+	else
+	{
+		if(nota>notaminima)
+		{
+			notaminima=nota;
+		}
+		if(nota<notamayor)
+		{
+			notamayor=nota;
+		}
+	}
+		
+}
+		
+}
+
+console.log(notamayor);
+console.log(notaminima);
 
 
-document.getElementById('suma').value=acumulador;
-document.getElementById('promedio').value=acumulador/contador;
-
-}//FIN DE LA FUNCIÓN
+//FIN DE LA FUNCIÓN
